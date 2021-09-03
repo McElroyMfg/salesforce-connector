4f574de00e15e216812a5881019c1b90d0ba546a
Edward <emmostrom@users.noreply.github.com>
2021-09-03 13:57:49 -0500

    add limit and offset do dialect

--- src/main/java/com/mcelroy/salesforceconnector/jdbc/SFDialect.java
+++ src/main/java/com/mcelroy/salesforceconnector/jdbc/SFDialect.java
@@ -5,4 +5,12 @@ package com.mcelroy.salesforceconnector.jdbc;
 import org.hibernate.dialect.Dialect;
 
 public class SFDialect extends Dialect {
+
+    public boolean supportsLimit() {
+        return true;
+    }
+
+    public String getLimitString(String sql, boolean hasOffset) {
+        return (new StringBuffer(sql.length() + 20)).append(sql).append(hasOffset ? " limit ? offset ?" : " limit ?").toString();
+    }
 }
