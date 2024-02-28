08eae01dc3465f46fb7787b73396dd26696cf202
Edward <emmostrom@users.noreply.github.com>
2024-02-27 19:53:56 -0600

    add simple unit test for PreparedStatement

--- src/test/java/com/mcelroy/salesforceconnector/JdbcTest.java
+++ src/test/java/com/mcelroy/salesforceconnector/JdbcTest.java
@@ -1,22 +1,25 @@
 package com.mcelroy.salesforceconnector;
 
+import com.mcelroy.salesforceconnector.jdbc.SFConnection;
 import com.mcelroy.salesforceconnector.parser.node.SQL_Statement;
 import com.mcelroy.salesforceconnector.parser.visitor.SOQL_Writer;
+import com.mcelroy.salesforceconnector.parser.visitor.SQL_Placeholder_Replacer;
 import com.mcelroy.salesforceconnector.parser.visitor.SQL_Visitor;
 import com.mcelroy.salesforceconnector.rest.SFClientConnection;
-import com.mcelroy.salesforceconnector.util.SFTestConnection;
 import org.json.JSONObject;
 import org.junit.Test;
 
 import java.sql.Date;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 public class JdbcTest {
 
@@ -28,16 +31,25 @@ public class JdbcTest {
         return b.toString();
     }
 
+    private String getQuery(String s, Map<Integer, String> values){
+        SQL_Statement sql_statement = SQL_Statement.parse(s);
+        StringBuilder b = new StringBuilder();
+        SQL_Visitor writer = new SQL_Placeholder_Replacer(new SOQL_Writer(b), values);
+        sql_statement.accept(writer);
+        return b.toString();
+    }
+
     Date getDate(String s) throws ParseException {
         SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd");
         return new Date(df.parse(s).getTime());
     }
 
     @Test
-    public void SimpleQueryTest() throws Exception{
+    public void StatementQueryTest() throws Exception{
 
         SFClientConnection clientConnection = mock(SFClientConnection.class);
-        SFTestConnection connection = new SFTestConnection(clientConnection);
+        SFConnection connection = spy(new SFConnection(null));
+        doReturn(clientConnection).when(connection).getClientConnection();
 
         JSONObject response = new JSONObject("{records: [{id: \"ID123\", name: \"Acme\", repId: 4, since: \"2024-01-30\"}, {id: \"ID456\", name: \"Stuff Inc.\", repId: 5, since: \"2023-08-04\"}]}");
         String sql = "select id, name, repId, since from Account";
@@ -60,5 +72,36 @@ public class JdbcTest {
         assertEquals(getDate("2024-01-30"), rs.getDate(4));
     }
 
+    @Test
+    public void PreparedStatementQueryTest() throws Exception{
 
+        SFClientConnection clientConnection = mock(SFClientConnection.class);
+        SFConnection connection = spy(new SFConnection(null));
+        doReturn(clientConnection).when(connection).getClientConnection();
+
+        JSONObject response = new JSONObject("{records: [{id: \"ID123\", name: \"Acme\", repId: 4, since: \"2024-01-30\"}, {id: \"ID456\", name: \"Stuff Inc.\", repId: 5, since: \"2023-08-04\"}]}");
+        String sql = "select id, name, repId, since from Account where repId > ? and since > ?";
+        Map<Integer, String> values = new HashMap<>();
+        values.put(1, "3");
+        values.put(2, "2022-01-01");
+
+        when(clientConnection.query(getQuery(sql, values))).thenReturn(response);
+
+        PreparedStatement statement = connection.prepareStatement(sql);
+        statement.setInt(1, 3);
+        statement.setDate(2, getDate("2022-01-01"));
+        statement.execute();
+        ResultSet rs = statement.getResultSet();
+
+        assertEquals(true, rs.next());
+        assertEquals("ID123", rs.getString("id"));
+        assertEquals(4, rs.getInt("repId"));
+        assertEquals("Acme", rs.getString("name"));
+        assertEquals(getDate("2024-01-30"), rs.getDate("since"));
+
+        assertEquals("ID123", rs.getString(1));
+        assertEquals(4, rs.getInt(3));
+        assertEquals("Acme", rs.getString(2));
+        assertEquals(getDate("2024-01-30"), rs.getDate(4));
+    }
 }
